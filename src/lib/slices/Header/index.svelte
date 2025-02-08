4c8cde7653b55f9e4133d59e6848ff97a67333d3
JanGaud <JanisGaudreault.jg@gmail.com>
2025-02-08 15:20:34 -0500

    Partially fixed mobile header

--- src/lib/slices/Header/index.svelte
+++ src/lib/slices/Header/index.svelte
@@ -13,40 +13,26 @@
 >
 	<!-- Grid Background -->
 	<div class="grid grid-cols-3 w-full h-full absolute inset-0">
-		<div
-			class="relative overflow-hidden opacity-30 hover:opacity-75 transition-opacity duration-200"
-		>
-			<PrismicImage
-				field={slice.primary.image_1}
-				class="w-full h-full object-cover object-center"
-			/>
+		<div class="relative overflow-hidden opacity-30 hover:opacity-75 transition-opacity duration-200">
+			<PrismicImage field={slice.primary.image_1} class="w-full h-full object-cover object-center" />
 		</div>
-		<div
-			class="relative overflow-hidden opacity-30 hover:opacity-75 transition-opacity duration-200"
-		>
-			<PrismicImage
-				field={slice.primary.image_2}
-				class="w-full h-full object-cover object-center"
-			/>
+		<div class="relative overflow-hidden opacity-30 hover:opacity-75 transition-opacity duration-200">
+			<PrismicImage field={slice.primary.image_2} class="w-full h-full object-cover object-center" />
 		</div>
-		<div
-			class="relative overflow-hidden opacity-30 hover:opacity-75 transition-opacity duration-200"
-		>
-			<PrismicImage
-				field={slice.primary.image_3}
-				class="w-full h-full object-cover object-center"
-			/>
+		<div class="relative overflow-hidden opacity-30 hover:opacity-75 transition-opacity duration-200">
+			<PrismicImage field={slice.primary.image_3} class="w-full h-full object-cover object-center" />
 		</div>
 	</div>
 
 	<!-- Text Content -->
 	<div
-		class="z-10 text-center space-y-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+		class="z-10 text-center space-y-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+		 px-4 sm:px-8 md:px-12 lg:px-16 w-full max-w-screen-lg pointer-events-none"
 	>
-		<h1 class="text-9xl font-bold tracking-wider shadow-lg">{slice.primary.titre}</h1>
-		<h2 class="text-7xl shadow-lg">{slice.primary.sous_titre}</h2>
-		<p class="text-2xl">{slice.primary.slogan}</p>
-		<div class="flex items-center justify-center gap-12 pt-8 pointer-events-auto">
+		<h1 class="text-6xl md:text-8xl lg:text-9xl font-bold tracking-wider shadow-lg">{slice.primary.titre}</h1>
+		<h2 class="text-3xl md:text-5xl lg:text-7xl shadow-lg">{slice.primary.sous_titre}</h2>
+		<p class="text-xl lg:text-2xl">{slice.primary.slogan}</p>
+		<div class="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-12 pt-8 pointer-events-auto">
 			{#each slice.primary.btn as link (link.key)}
 				<PrismicLink
 					class="border-2 py-2 px-4 border-black bg-white text-black rounded-3xl uppercase"
