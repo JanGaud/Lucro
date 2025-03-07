e62d2eae98f929d2a6f0685fded2db56c35f7821
JanGaud <JanisGaudreault.jg@gmail.com>
2025-03-07 17:42:53 -0500

    meta img try to fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { PrismicPreview } from '@prismicio/svelte/kit';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	import { page } from '$app/state';
+	import { page } from '$app/stores';
 	import { repositoryName } from '$lib/prismicio';
 	import '../app.css';
 	import Navigation from '$lib/components/Navigation.svelte';
@@ -14,20 +14,20 @@
 </script>
 
 <svelte:head>
-	<link rel="canonical" href="{page.url.toString()}" />
-	{#if page.data}
-		<title>{page.data.meta_title || 'Default Title'}</title>
+	<link rel="canonical" href="{$page.url.toString()}" />
+	{#if $page.data}
+		<title>{$page.data.meta_title || 'Default Title'}</title>
 
-		{#if page.data.meta_description}
-			<meta name="description" content={page.data.meta_description} />
+		{#if $page.data.meta_description}
+			<meta name="description" content={$page.data.meta_description} />
 		{/if}
 
-		{#if page.data.meta_title}
-			<meta name="og:title" content={page.data.meta_title} />
+		{#if $page.data.meta_title}
+			<meta name="og:title" content={$page.data.meta_title} />
 		{/if}
 
-		{#if page.data.meta_image}
-			<meta name="og:image" content={page.data.meta_image} />
+		{#if $page.data.meta_image}
+			<meta name="og:image" content={$page.data.meta_image.url} />
 			<meta name="twitter:card" content="summary_large_image" />
 		{/if}
 	{/if}
